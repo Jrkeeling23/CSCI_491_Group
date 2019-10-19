dc7a91b50d716b368dc7f4b9ca1c61600e7864a3
Alex Harry <alex.harry1@gmail.com>
2019-10-19 16:54:19 -0600

    Added method to get best neighbor based on accuracy

--- Code/main.py
+++ Code/main.py
@@ -12,23 +12,28 @@ class Main():
         # Run machine learning algorithms
         self.algorithm.naive_bays()
         self.algorithm.KNN(23)
-        # heighest_acc = 0.0
-        # tuned_neighbors = 0
-        # neighbors = []
-        # accuracy = []
-        # for k_neighbors in range(1, 25):
-        #     acc = self.algorithm.KNN(k_neighbors)
-        #     print("Neighbors: ", k_neighbors, "Accuracy ", acc)
-        #     neighbors.append(k_neighbors)
-        #     accuracy.append(acc)
-        #
-        #     if acc > heighest_acc:
-        #         heighest_acc = acc
-        #         tuned_neighbors = k_neighbors
-        # print("Highest accuracy neighbor count: ", tuned_neighbors, " with accuracy ", heighest_acc)
+        # self.get_best_k_neightbor()
 
-        #
-        # plt.plot(neighbors, accuracy)
-        # plt.show()
+    def get_best_k_neightbor(self):
+        heighest_acc = 0.0
+        tuned_neighbors = 0
+        neighbors = []
+        accuracy = []
+        for k_neighbors in range(1, 25):
+            acc = self.algorithm.KNN(k_neighbors)
+            print("Neighbors: ", k_neighbors, "Accuracy ", acc)
+            neighbors.append(k_neighbors)
+            accuracy.append(acc)
+
+            if acc > heighest_acc:
+                heighest_acc = acc
+                tuned_neighbors = k_neighbors
+        print("Highest accuracy neighbor count: ", tuned_neighbors, " with accuracy ", heighest_acc)
+
+
+        plt.plot(neighbors, accuracy)
+        plt.show()
+
+
 if __name__ == "__main__":
     Main()
